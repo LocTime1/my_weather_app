7da248421e2ae7cd9253d6ee5e82253485dbfc6d
Gavrilov Alexander <sashagavrilov220307@gmail.com>
2024-10-31 12:51:48 +0300

    продолжаю делать appbar

--- lib/widgets/my_appbar.dart
+++ lib/widgets/my_appbar.dart
@@ -1,34 +1,46 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 
 class MyAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Container(
       color: Colors.green,
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.only(
-          right: MediaQuery.of(context).size.width * 0.07,
-          top: 140,
-          left: MediaQuery.of(context).size.width * 0.07),
+      width: width,
+      margin:
+          EdgeInsets.only(right: width * 0.07, top: 140, left: width * 0.07),
       child: Row(
         children: [
-          GFButton(
-            onPressed: () {},
-            color: Colors.black,
-            child: Container(
-              color: Colors.yellow,
-              child: Row(
-                children: [
-                  Image.asset("assets/icons/my_location.png"),
-                  Text(
-                    "AAAAAAAAA",
-                    style: TextStyle(fontSize: 5),
-                  )
-                ],
+          Container(
+            width: width * 0.335,
+            child: GFButton(
+              onPressed: () {},
+              color: Colors.transparent,
+              borderSide: BorderSide(color: Colors.black, width: 1.0),
+              shape: GFButtonShape.pills,
+              icon: Image.asset(
+                "assets/icons/my_location.png",
+                width: width * 0.056,
               ),
+              text: "Определить моё\n местоположение",
+              textStyle: TextStyle(fontSize: width * 0.025),
+              textColor: Colors.white,
+              // child: Container(
+              //   color: Colors.yellow,
+              // child: Row(
+              //   children: [
+              //     Image.asset("assets/icons/my_location.png"),
+              //     Text(
+              //       "AAAAAAAAA",
+              //       style: TextStyle(fontSize: 5),
+              //     )
+              //   ],
+              // ),
             ),
-          )
+          ),
         ],
       ),
     );
